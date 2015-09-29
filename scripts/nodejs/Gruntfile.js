4786181f2cb903525542f267eaaee3325be4ecac
Thibault Kruse <thibault.kruse@gmx.de>
2015-09-29 11:24:28 +0200

    Use grunt and deadlink to find dead external links

---
+++ scripts/nodejs/Gruntfile.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    deadlink: {
+      options: {
+      },
+      target: {
+        src: [ "../../*.md" ] // glob pattern. files path that include links to checking.
+      },
+    },
+  });
+  grunt.loadNpmTasks('grunt-deadlink');
+  grunt.registerTask('test', ['deadlink:target']);
+}
+
